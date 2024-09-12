0331f40f9bee5de3d5535c9f1dc465ac6e18e86b
holaleche <whoisleche@gmail.com>
2024-09-12 14:42:07 +0900

    Setting boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default App;
