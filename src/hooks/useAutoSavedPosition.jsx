b5b8c6dc6d9b490a6c672289d50e616f58f97cf3
holaleche <whoisleche@gmail.com>
2024-10-10 19:31:06 +0900

    Refactor 자동 저장 데이터

--- src/hooks/useAutoSavedPosition.jsx
+++ src/hooks/useAutoSavedPosition.jsx
@@ -6,16 +6,28 @@ import useModelStore from "../stores/useModelStore";
 const useAutoSavedPosition = () => {
   const { rotations, positions, autoSetPositions } = useModelStore();
 
+  useEffect(() => {
+    const savedPositions = JSON.parse(localStorage.getItem("positions"));
+    const savedRotations = JSON.parse(localStorage.getItem("rotations"));
+
+    if (savedPositions && savedRotations) {
+      autoSetPositions(savedPositions, savedRotations);
+    }
+  }, [autoSetPositions]);
+
   const positionsRef = useRef(positions);
   const rotationsRef = useRef(rotations);
 
   useEffect(() => {
     const interval = setInterval(() => {
       if (
-        positions !== positionsRef.current &&
+        positions !== positionsRef.current ||
         rotations !== rotationsRef.current
       ) {
         autoSetPositions(positions, rotations);
+
+        localStorage.setItem("positions", JSON.stringify(positions));
+        localStorage.setItem("rotations", JSON.stringify(rotations));
 
         positionsRef.current = positions;
         rotationsRef.current = rotations;
