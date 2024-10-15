3f95a4671b277d0a47e0be06ab28a453e6f70856
holaleche <whoisleche@gmail.com>
2024-10-15 22:14:33 +0900

    Refactor 자동 저장
    Refactor custom hook
    
    Refactor stores
    
    Refactor utils
    
    Refactor components

--- src/stores/useAuthStore.js
+++ src/stores/useAuthStore.js
@@ -1,11 +1,7 @@
 import { create } from "zustand";
 
 const useAuthStore = create((set) => ({
-  errorMessage: "",
-
   setIsLoggedIn: (loggedIn) => set({ isLoggedIn: loggedIn }),
-
-  setErrorMessage: (message) => set({ errorMessage: message }),
 }));
 
 export default useAuthStore;
