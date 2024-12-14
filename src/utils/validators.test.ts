67c7effff9e1b18a302142b876613c3a14d5b879
holaleche <whoisleche@gmail.com>
2024-12-14 17:58:59 +0900

    Fix 스피커 배치 정보 비교 함수 (Squash)
    Type 위치 값, 회전 값 통합 타입 정의
    
    Test 비교 유틸 함수

--- src/utils/validators.test.ts
+++ src/utils/validators.test.ts
@@ -1,29 +1,29 @@
 import { describe, it, expect } from "vitest";
 
 import {
-  comparePositions,
+  compareTransformation,
   isDuplicateData,
   isValidateNumber,
   hasCurrentRef,
 } from "../utils/validators";
-import type { Positions, Rotations, UserData } from "../types/common";
+import type { Transformation, UserData } from "../types/common";
 
-const position1: Positions = {
+const position1: Transformation = {
   firstSpeakerPosition: [1.0, 2.0, 3.0],
   secondSpeakerPosition: [4.0, 5.0, 6.0],
   listenerPosition: [7.0, 8.0, 9.0],
 };
 
-const position2: Positions = {
+const position2: Transformation = {
   firstSpeakerPosition: [1.0, 2.0, 3.0],
   secondSpeakerPosition: [4.0, 5.0, 6.0],
   listenerPosition: [7.0, 8.0, 9.0],
 };
 
-const position3: Positions = {
-  firstSpeakerPosition: [1.0, 2.1, 3.0],
-  secondSpeakerPosition: [4.0, 5.0, 6.0],
-  listenerPosition: [7.0, 8.0, 9.0],
+const currentPosition: Transformation = {
+  firstSpeaker: [1.0, 2.1, 3.0],
+  secondSpeaker: [4.0, 5.0, 6.0],
+  listener: [7.0, 8.0, 9.0],
 };
 
 const userData1: UserData[] = [
@@ -40,40 +40,44 @@ const userData1: UserData[] = [
   },
 ];
 
-describe("comparePositions function", () => {
+describe("compareTransformation function", () => {
   it("should return true for identical positions", () => {
-    expect(comparePositions(position1, position2)).toBe(true);
+    expect(compareTransformation(position1, position2)).toBe(true);
   });
 
   it("should return false for different positions", () => {
-    expect(comparePositions(position1, position3)).toBe(false);
+    expect(compareTransformation(position1, currentPosition)).toBe(false);
   });
 
   it("should handle empty positions", () => {
-    expect(comparePositions({}, {})).toBe(true);
-    expect(comparePositions({}, position1)).toBe(false);
+    expect(compareTransformation({}, {})).toBe(true);
+    expect(compareTransformation({}, position1)).toBe(false);
   });
 });
 
 describe("isDuplicateData function", () => {
   it("should return true for identical positions and rotations", () => {
-    const rotation1: Rotations = {
-      firstSpeakerRotation: [1.0, 2.1, 3.0],
-      secondSpeakerRotation: [4.0, 5.0, 6.0],
-      listenerRotation: [7.0, 8.0, 9.0],
+    const currentRotation: Transformation = {
+      firstSpeaker: [1.0, 2.1, 3.0],
+      secondSpeaker: [4.0, 5.0, 6.0],
+      listener: [7.0, 8.0, 9.0],
     };
 
-    expect(isDuplicateData(userData1, position3, rotation1)).toBe(true);
+    expect(isDuplicateData(userData1, currentPosition, currentRotation)).toBe(
+      true,
+    );
   });
 
   it("should return false for different positions or rotations", () => {
-    const rotation2: Rotations = {
-      firstSpeakerRotation: [0.0, 0.0, 0.0],
-      secondSpeakerRotation: [4.0, 5.0, 6.0],
-      listenerRotation: [7.0, 8.0, 9.0],
+    const currentRotation: Transformation = {
+      firstSpeaker: [0.0, 0.0, 0.0],
+      secondSpeaker: [4.0, 5.0, 6.0],
+      listener: [7.0, 8.0, 9.0],
     };
 
-    expect(isDuplicateData(userData1, position3, rotation2)).toBe(false);
+    expect(isDuplicateData(userData1, currentPosition, currentRotation)).toBe(
+      false,
+    );
   });
 });
 
